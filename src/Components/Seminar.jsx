91a15389979c00416aed29ca255f28393e34d8cb
DashaIgnatieva <dariyignatieva@mail.ru>
2025-02-06 19:51:22 +0300

    Added bootstrap styles for seminar card

--- src/Components/Seminar.jsx
+++ src/Components/Seminar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
+
+import trashIcon from '../assets/trash.svg';
+import pencilIcon from '../assets/pencil.svg';
 
 const Seminar = (props) => {
     const {
@@ -20,20 +23,20 @@ const Seminar = (props) => {
     }
 
     return (
-        <div className='seminar-card'>
-            <div className='seminar-date'>
-                <p>{time}</p>
+        <Row className='seminar-card d-flex flex-row justify-content-between mb-2 border-bottom'>
+            <Col className='d-flex flex-column justify-content-center'>
+                <p className="fs-2 mb-0">{time}</p>
                 <p>{date}</p>
-            </div>
-            <div className='seminar-info'>
-                <h3>{title}</h3>
-                <p>{description}</p>
-            </div>
-            <div className='buttons'>
-                <Button className='bi bi-trash' onClick={deleteSeminar}></Button>
-                <Button className='bi bi-pencil' onClick={editSeminar}></Button>
-            </div>
-        </div>
+            </Col>
+            <Col className='d-flex flex-column justify-content-center' xs={8}>
+                <h4 className='text-start'>{title}</h4>
+                <p className='text-start'>{description}</p>
+            </Col>
+            <Col className='d-flex align-items-center justify-content-center'>
+                <Button variant="outline-dark" onClick={deleteSeminar}><img src={trashIcon} alt="Удалить"></img></Button>
+                <Button variant="outline-dark" className='ms-1' onClick={editSeminar}><img src={pencilIcon} alt="Редактировать"></img></Button>
+            </Col>
+        </Row>
     );
 }
 
