1133c2a807b55d0d66bfd5f1d485a4b604986904
DashaIgnatieva <dariyignatieva@mail.ru>
2025-02-08 12:31:25 +0300

    preparing for server deployment

---
+++ mock-server/server.cjs
@@ -0,0 +1,26 @@
+const jsonServer = require('json-server');
+const fs = require('fs')
+const path = require('path')
+
+const server = jsonServer.create()
+
+const filePath = path.join('mock-server', 'seminars.json');
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}));
+
+server.use(router);
+server.listen(3000, () => {
+    console.log('JSON Server is running')
+});
+
+module.exports = server;
