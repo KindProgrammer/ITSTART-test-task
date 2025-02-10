682d750fd8ca9900dd7a10a42002d7107e58333b
DashaIgnatieva <dariyignatieva@mail.ru>
2025-02-11 01:09:51 +0300

    Fixed cors

--- api/server.js
+++ api/server.js
@@ -1,9 +1,7 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 
 const server = jsonServer.create()
 
-// Uncomment to allow write operations
 const fs = require('fs')
 const path = require('path')
 const filePath = path.join('db.json')
@@ -11,18 +9,22 @@ const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db)
 
-// Comment out to allow write operations
-// const router = jsonServer.router('db.json')
-
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
+
+// CORS
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+});
+
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
